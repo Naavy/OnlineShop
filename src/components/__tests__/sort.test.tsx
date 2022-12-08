b99f5df8e40f8e627471cbe9df10ad84f5c1b280
Natalia Niesyt <natalia.niesyt@gmail.com>
2022-12-08 11:15:35 +0100

    Add objectSorting

--- src/components/__tests__/sort.test.tsx
+++ src/components/__tests__/sort.test.tsx
@@ -1,17 +1,38 @@
 export const idx = require('../sort.js')
 
 const TEST_ARRAY = ["chain", "backpack", "tablecloth", "bag"]
+const TEST_ARRAY_2 = [
+  { id: 1, title: "T-Shirt", price: 22.3 },
+  { id: 2, title: "Backpack", price: 109.95 },
+  { id: 3, title: "Jacket", price: 55.99 },
+  { id: 4, title: "SanDisk SSD", price: 109 },
+  { id: 5, title: "Bracelet", price: 695 },
+]
 
 describe("Sort function test", () => {
-  it('sort ascending', () => {
-    expect(idx.sorting(TEST_ARRAY, 'asc')).toStrictEqual(["backpack", "bag", "chain", "tablecloth"]);
+  it('simple sort ascending', () => {
+    expect(idx.simpleSorting(TEST_ARRAY, 'asc')).toStrictEqual(["backpack", "bag", "chain", "tablecloth"]);
   });
 
-  it('sort descending', () => {
-    expect(idx.sorting(TEST_ARRAY, 'desc',)).toStrictEqual(["tablecloth", "chain", "bag", "backpack"]);
+  it('simple sort descending', () => {
+    expect(idx.simpleSorting(TEST_ARRAY, 'desc',)).toStrictEqual(["tablecloth", "chain", "bag", "backpack"]);
   });
 
   it('bad argument for simple sorting', () => {
-    expect(idx.sorting(TEST_ARRAY, 'ascc')).toBe('Error');
+    expect(idx.simpleSorting(TEST_ARRAY, 'ascc')).toBe('Error');
+  });
+
+  it('sort object ascending', () => {
+    expect(idx.objectSorting(TEST_ARRAY_2, 'asc')[0].title).toEqual("Backpack");
+    expect(idx.objectSorting(TEST_ARRAY_2, 'asc')[TEST_ARRAY_2.length - 1].title).toEqual("T-Shirt");
+  });
+
+  it('sort object descending', () => {
+    expect(idx.objectSorting(TEST_ARRAY_2, 'desc')[0].title).toEqual("T-Shirt");
+    expect(idx.objectSorting(TEST_ARRAY_2, 'desc')[TEST_ARRAY_2.length - 1].title).toEqual("Backpack");
+  });
+
+  it('bad argument for object sorting', () => {
+    expect(idx.objectSorting(TEST_ARRAY_2, 'descc')).toBe('Error');
   });
 });
--- src/components/sort.js
+++ src/components/sort.js
@@ -1,5 +1,25 @@
-export const sorting = (array, way) => {
-  if (way === 'asc') return array.sort()
-  if (way === 'desc') return array.reverse()
-  return 'Error'
+export const simpleSorting = (array, way) => {
+  if (way !== 'asc' && way !== 'desc') return 'Error'
+  if (way === 'asc') array.sort()
+  if (way === 'desc') array.reverse()
+  return array
+}
+
+export const objectSorting = (array, way) => {
+  if (way !== 'asc' && way !== 'desc') return 'Error'
+  if (way === 'asc') {
+    array.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    })
+  }
+  if (way === 'desc') {
+    array.sort((a, b) => {
+      if (a.title < b.title) return 1;
+      if (a.title > b.title) return -1;
+      return 0;
+    });
+  }
+  return array
 }
